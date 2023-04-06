ff37edf8a9b1aa770c08963827541c3cbe56afbd
YVKG <yvkg@qq.com>
2023-04-06 18:11:57 +0800

    commit

---
+++ website-backend/src/test/java/com/qinweizhao/website/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
